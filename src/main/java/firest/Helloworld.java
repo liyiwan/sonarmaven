f4929020ea109c256e5440c5a4629378d7a5705e
hcx <hcx@hcx-PC>
2015-06-07 10:56:42 +0800

    1

--- src/main/java/firest/Helloworld.java
+++ src/main/java/firest/Helloworld.java
@@ -3,6 +3,7 @@ package firest;
 public class Helloworld {
 	 public static void main (String args[]){
 		    System.out.println("Hello World!");
+		    System.out.println("Hello World1!");
 		}
 
 }
